7791fb7ed5f264d1626e23997b6599001cbc45e7
Puzik0 <135577653+Puzik0@users.noreply.github.com>
2023-07-16 00:26:36 +0300

    Added Gym State Entry

---
+++ Assets/Scripts/Infrastructure/Bootstrap.cs
@@ -0,0 +1,15 @@
+﻿using GameStates.Base;
+using GameStates.States;
+using System.Collections;
+using UnityEngine;
+
+namespace Infrastructure
+{
+    public class Bootstrap : MonoBehaviour
+    {
+        [SerializeField] private GameStateMachineSo _stateMachine;
+
+        private void OnEnable() => 
+            _stateMachine.Enter<EnterGymStateSo>();
+    }
+}
