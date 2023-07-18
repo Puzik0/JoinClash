d763d7d47ee2fc8a04c395c7fbede8645faa9933
Puzik0 <135577653+Puzik0@users.noreply.github.com>
2023-07-18 21:54:59 +0300

    added wallet

---
+++ Assets/Sources/Model/Currency/Coin.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Model.Currency
+{
+    public class Coin : MonoBehaviour
+    {
+        public readonly int Par;
+
+        public Coin(int par)
+        {
+            Model.Currency.Wallet.CheckBalanceOnNegative(par);
+
+            Par = par;
+        }
+    }
+}
---
+++ Assets/Sources/Model/Currency/Wallet.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using UnityEngine;
+
+namespace Model.Currency
+{
+    public class Wallet : MonoBehaviour
+    {
+        public Wallet(int balance)
+        {
+            CheckBalanceOnNegative(balance);
+            Balance = balance;
+        }
+        public event Action Changed;
+        
+
+        public int Balance {  get; private set; }
+
+        public void Add(int balance)
+        {
+            CheckBalanceOnNegative(balance);
+            Balance += balance;
+            Changed?.Invoke();
+        }
+        public void TrySpend(int balance)
+        {
+            CheckBalanceOnNegative(balance);
+
+            if (Balance < balance)
+                return;
+
+            Balance -= balance;
+            Changed?.Invoke();
+
+        }
+        public static void CheckBalanceOnNegative(int balance)
+        {
+            if (balance < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(balance));
+
+                //Debug.LogWarning("Ппопытка прибавить отрицательный баланс. приравнен к 0 ");
+                //balance = 0;
+            }
+        }
+
+    }
+}
